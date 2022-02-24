daeafc70137bbe61cd2e3247583a5031ac7782ea
JeanCharpentier <jeancharpentier@protonmail.com>
2022-02-24 11:00:45 +0100

    Tilemaped !

---
+++ TileMap.lua
@@ -0,0 +1,53 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.8.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 11,
+  tilewidth = 64,
+  tileheight = 64,
+  nextlayerid = 2,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "KenneyTanks",
+      firstgid = 1,
+      filename = "../Ressources/KenneyTanks.tsx"
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 11,
+      id = 1,
+      name = "Calque de Tuiles 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+        1, 14, 3, 3, 7, 3, 3, 15, 1, 1, 8, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+        1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 8, 21, 31, 34, 23, 23, 23, 23, 35, 21,
+        1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 8, 21, 21, 22, 21, 21, 21, 21, 22, 21,
+        1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 8, 21, 21, 22, 21, 21, 21, 21, 22, 21,
+        1, 2, 1, 1, 4, 3, 3, 12, 3, 3, 28, 23, 23, 25, 21, 21, 21, 31, 22, 21,
+        1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 8, 21, 21, 22, 21, 21, 34, 23, 37, 21,
+        1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 8, 21, 21, 22, 21, 21, 22, 21, 21, 21,
+        1, 2, 1, 1, 4, 3, 3, 17, 1, 1, 8, 21, 21, 22, 21, 21, 22, 21, 21, 21,
+        1, 16, 3, 3, 17, 1, 1, 1, 1, 1, 8, 21, 31, 22, 21, 21, 22, 31, 21, 21,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 21, 21, 36, 23, 23, 37, 21, 21, 21
+      }
+    }
+  }
+}
